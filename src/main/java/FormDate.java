cb48992a7ee2c279d3df4b150991f9ada18eb554
GorLena <zhura1977@list.ru>
2024-07-22 16:17:01 +0300

    rework

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -1,8 +1,7 @@
 public class FormDate {
-    public FormDate birthday;
 
-        public int day;
-        public int month;
-        public int  year;
-    }
+    public int day;
+    public int month;
+    public int year;
+}
 
